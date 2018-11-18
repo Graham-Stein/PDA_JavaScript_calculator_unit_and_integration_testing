078e4d3ba83a56fae23dd3914406bbc37005085f
Graham-Stein <graham@guavoaviation.co.uk>
2018-11-18 10:12:11 +0000

    Add basic protractor test

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -16,4 +16,8 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it('number buttons should update the display of the running total', function() {
+    
+  });
+
 });
